7884bbb3aa1c6ae16a4fccdbe28d0590c8b54105
Niek Visser <niek@bcome.nl>
2018-01-25 17:35:55 +0100

    Add docblocks

--- app/Helpers/StringHelper.php
+++ app/Helpers/StringHelper.php
@@ -1,9 +1,18 @@
 <?php
 
 namespace App\Helpers;
-
+/**
+ * Class StringHelper
+ * @package App\Helpers
+ */
 class StringHelper
 {
+    /**
+     * Generate a pseudorandom string
+     *
+     * @param int $len
+     * @return string
+     */
     public static function getRandom(int $len = 32): string
     {
         return substr(bin2hex(random_bytes(32)), 0 , $len);
